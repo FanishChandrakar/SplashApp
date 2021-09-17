a9f39d01b86119342952ccd5e432d74d549a7576
FanishChandrakar <FanishChandrakar>
2021-09-17 14:40:09 +0530

    rn-splash-screen

--- android/app/src/main/java/com/splashapp/MainActivity.java
+++ android/app/src/main/java/com/splashapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.splashapp;
 
 import com.facebook.react.ReactActivity;
+// react-native-splash-screen
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +14,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "SplashApp";
+  }
+
+  // react-native-splash-screen
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);  // here
+    super.onCreate(savedInstanceState);
   }
 }
